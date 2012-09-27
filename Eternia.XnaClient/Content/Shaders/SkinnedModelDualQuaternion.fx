9976046ebe89030963279eeee058395b7c81e3e6
myko <myko@bredband.net>
2012-09-27 11:04:15 +0200

    c

---
+++ Eternia.XnaClient/Content/Shaders/SkinnedModelDualQuaternion.fx
@@ -0,0 +1,210 @@
+//-----------------------------------------------------------------------------
+// SkinnedModel.fx
+//
+// Microsoft Game Technology Group
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+
+//This code contains modified code from: http://isg.cs.tcd.ie/kavanl/dq/dqs.cg
+//As such, it keeps the original code's license:
+/* dqs.cg
+
+  Dual quaternion skinning vertex shader (unoptimized version, no shading computations)
+
+  Version 1.0.0, February 7th, 2007
+
+  Copyright (C) 2006-2007 University of Dublin, Trinity College, All Rights 
+  Reserved
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the author(s) be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Author: Ladislav Kavan, kavanl@cs.tcd.ie
+
+*/
+
+
+// Maximum number of bone matrices we can render using shader 2.0 in a single pass.
+// If you change this, update SkinnedModelProcessor.cs to match.
+// Note: This number could theoretically be increased to 59 * 2 because dual quaternions take up half of the constant registers that matrices do.
+#define MaxBones 115
+
+
+// Input parameters.
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+
+float4 Bones[MaxBones * 2];
+
+float3 Light1Direction = normalize(float3(0.5, -1, -0.2));
+float3 Light1Color = float3(1, 0.9, 0.7);
+
+float3 Light2Direction = normalize(float3(-0.3, 1, 0.1));
+float3 Light2Color = float3(1, 0.9, 0.7);
+
+float3 AmbientColor = 0.3;
+float3 DiffuseColor = 1;
+
+texture Texture;
+
+sampler Sampler = sampler_state
+{
+    Texture = (Texture);
+
+    MinFilter = Linear;
+    MagFilter = Linear;
+    MipFilter = Linear;
+};
+
+
+// Vertex shader input structure.
+struct VS_INPUT
+{
+    float4 Position : POSITION0;
+    float3 Normal : NORMAL0;
+    float2 TexCoord : TEXCOORD0;
+    float4 BoneIndices : BLENDINDICES0;
+    float4 BoneWeights : BLENDWEIGHT0;
+};
+
+
+// Vertex shader output structure.
+struct VS_OUTPUT
+{
+    float4 Position : POSITION0;
+    float3 Lighting : COLOR0;
+    float2 TexCoord : TEXCOORD0;
+};
+
+float4x4 DQToMatrix(float4 Qn, float4 Qd)
+{	
+	Matrix M;
+    
+    float len2 = dot(Qn, Qn);
+    float w = Qn.w, x = Qn.x, y = Qn.y, z = Qn.z;
+    float t0 = Qd.w, t1 = Qd.x, t2 = Qd.y, t3 = Qd.z;
+				
+    M[0][0] = w*w + x*x - y*y - z*z;
+    M[1][0] = 2 * x * y - 2 * w * z;
+    M[2][0] = 2 * x * z + 2 * w * y;
+    M[0][1] = 2 * x * y + 2 * w * z;
+    M[1][1] = w * w + y * y - x * x - z * z;
+    M[2][1] = 2 * y * z - 2 * w * x;
+    M[0][2] = 2 * x * z - 2 * w * y;
+    M[1][2] = 2 * y * z + 2 * w * x;
+    M[2][2] = w * w + z * z - x * x - y * y;
+
+    M[3][0] = -2 * t0 * x + 2 * w * t1 - 2 * t2 * z + 2 * y * t3;
+    M[3][1] = -2 * t0 * y + 2 * t1 * z - 2 * x * t3 + 2 * w * t2;
+    M[3][2] = -2 * t0 * z + 2 * x * t2 + 2 * w * t3 - 2 * t1 * y;
+
+    M[0][3] = 0;
+    M[1][3] = 0;
+    M[2][3] = 0;
+    M[3][3] = len2;
+
+    M /= len2;
+
+    return M;	
+}
+
+// Vertex shader program.
+VS_OUTPUT VertexShader(VS_INPUT input)
+{
+    VS_OUTPUT output;
+    
+    float2x4 blendDQ;
+    
+    /*blendDQ[0] = Bones[input.BoneIndices.x * 2];
+    blendDQ[1] = Bones[input.BoneIndices.x * 2 + 1];*/
+    
+    float yc = 1.0, zc = 1.0, wc = 1.0;
+    
+    if (dot(Bones[input.BoneIndices.x * 2], Bones[input.BoneIndices.y * 2]) < 0.0)
+    {
+		yc = -1.0;
+    }
+    if (dot(Bones[input.BoneIndices.x * 2], Bones[input.BoneIndices.z * 2]) < 0.0)
+    {
+		zc = -1.0;
+	}
+    if (dot(Bones[input.BoneIndices.x * 2], Bones[input.BoneIndices.w * 2]) < 0.0)
+    {
+		wc = -1.0;
+	}
+    
+    blendDQ[0] = Bones[input.BoneIndices.x * 2] * input.BoneWeights.x;
+    blendDQ[1] = Bones[input.BoneIndices.x * 2 + 1] * input.BoneWeights.x;
+    
+    blendDQ[0] += yc*Bones[input.BoneIndices.y * 2] * input.BoneWeights.y;
+    blendDQ[1] += yc*Bones[input.BoneIndices.y * 2 + 1] * input.BoneWeights.y;
+    
+    blendDQ[0] += zc*Bones[input.BoneIndices.z * 2] * input.BoneWeights.z;
+    blendDQ[1] += zc*Bones[input.BoneIndices.z * 2 + 1] * input.BoneWeights.z;
+    
+    blendDQ[0] += wc*Bones[input.BoneIndices.w * 2] * input.BoneWeights.w;
+    blendDQ[1] += wc*Bones[input.BoneIndices.w * 2 + 1] * input.BoneWeights.w;
+    
+    // Blend between the weighted bone matrices.
+    float4x4 skinTransform = DQToMatrix(blendDQ[0], blendDQ[1]);
+    
+    // Skin the vertex position.
+    float4 position = mul(input.Position, skinTransform);
+    
+    output.Position = mul(mul(mul(position, World), View), Projection);
+
+    // Skin the vertex normal, then compute lighting.
+    float3 normal = normalize(mul(mul(input.Normal, skinTransform), World));
+    
+    float3 light1 = max(dot(normal, Light1Direction), 0) * Light1Color;
+    float3 light2 = max(dot(normal, Light2Direction), 0) * Light2Color;
+
+    output.Lighting = DiffuseColor * (light1 + light2) + AmbientColor;
+
+    output.TexCoord = input.TexCoord;
+    
+    return output;
+}
+
+
+// Pixel shader input structure.
+struct PS_INPUT
+{
+    float3 Lighting : COLOR0;
+    float2 TexCoord : TEXCOORD0;
+};
+
+
+// Pixel shader program.
+float4 PixelShader(PS_INPUT input) : COLOR0
+{
+    float4 color = tex2D(Sampler, input.TexCoord);
+
+    color.rgb *= input.Lighting;
+    
+    return color;
+}
+
+
+technique SkinnedModelTechnique
+{
+    pass SkinnedModelPass
+    {
+        VertexShader = compile vs_2_0 VertexShader();
+        PixelShader = compile ps_2_0 PixelShader();
+    }
+}
---
+++ Eternia.XnaClient/Controls/AbilityTooltip.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using EterniaGame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Myko.Xna.Ui;
+using System.Collections.Generic;
+using EterniaGame.Abilities;
+using EterniaGame.Actors;
+
+namespace EterniaXna
+{
+    public class AbilityTooltip: Control
+    {
+        private class Line
+        {
+            public Color Color { get; set; }
+            public string Text { get; set; }
+        }
+
+        private Binding<Actor> actorBinding;
+        private Ability ability;
+        private List<Line> lines;
+
+        public AbilityTooltip(Binding<Actor> actor, Ability ability)
+        {
+            this.actorBinding = actor;
+            this.ability = ability;
+            lines = new List<Line>();
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Width = 160;
+
+            var actor = actorBinding.GetValue();
+
+            if (actor != null)
+            {
+                lines.Clear();
+
+                int abilityDamageUpper = (int)((actor.CurrentStatistics.AttackPower * ability.Damage.AttackPowerScale + actor.CurrentStatistics.SpellPower * ability.Damage.SpellPowerScale + ability.Damage.Value) * actor.CurrentStatistics.DamageDone);
+                int abilityDamageLower = (int)((abilityDamageUpper * actor.CurrentStatistics.Precision) * actor.CurrentStatistics.DamageDone);
+                float averageDamage = ((abilityDamageLower + abilityDamageUpper) / 2.0f) * (actor.CurrentStatistics.CritChance + 1.0f);
+
+                int abilityHealingUpper = (int)((actor.CurrentStatistics.AttackPower * ability.Healing.AttackPowerScale + actor.CurrentStatistics.SpellPower * ability.Healing.SpellPowerScale + ability.Healing.Value) * actor.CurrentStatistics.HealingDone);
+                int abilityHealingLower = (int)((abilityHealingUpper * actor.CurrentStatistics.Precision) * actor.CurrentStatistics.HealingDone);
+                float averageHealing = ((abilityHealingLower + abilityHealingUpper) / 2.0f) * (actor.CurrentStatistics.CritChance + 1.0f);
+
+                var damageString = ability.Damage.Value.ToString("0") + " " + ability.Damage.School.ToString() + " damage";
+                if (ability.Damage.SpellPowerScale > 0f)
+                    damageString = ability.Damage.SpellPowerScale.ToString("#0%") + " spell power + " + damageString;
+                if (ability.Damage.AttackPowerScale > 0f)
+                    damageString = ability.Damage.AttackPowerScale.ToString("#0%") + " attack power + " + damageString;
+
+                var healingString = ability.Healing.Value.ToString("0") + " healing";
+                if (ability.Healing.SpellPowerScale > 0f)
+                    healingString = ability.Healing.SpellPowerScale.ToString("#0%") + " spell power + " + healingString;
+                if (ability.Healing.AttackPowerScale > 0f)
+                    healingString = ability.Healing.AttackPowerScale.ToString("#0%") + " attack power + " + healingString;
+
+                lines.Add(new Line { Color = Color.LightGray, Text = ability.Description });
+                lines.Add(new Line { Color = Color.LightGray, Text = ability.DamageType.ToString() });
+
+                if (ability.Damage.Value > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = damageString });
+                if (ability.Healing.Value > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = healingString });
+
+                if (ability.ManaCost > 0)
+                    lines.Add(new Line { Color = actor.CurrentMana >= ability.ManaCost ? Color.LightGray : Color.Tomato, Text = ability.ManaCost.ToString() + " mana" });
+                if (ability.EnergyCost > 0)
+                    lines.Add(new Line { Color = actor.CurrentEnergy >= ability.EnergyCost ? Color.LightGray : Color.Tomato, Text = ability.EnergyCost.ToString() + " energy" });
+                if (ability.EnergyCost < 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = "Generates " + (-ability.EnergyCost).ToString() + " energy" });
+                if (ability.Damage.Value > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = abilityDamageLower.ToString() + " - " + abilityDamageUpper.ToString() + " damage" });
+                if (ability.Healing.Value > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = abilityHealingLower.ToString() + " - " + abilityHealingUpper.ToString() + " healing" });
+                if (ability.Cooldown.Duration > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = ability.Cooldown.Duration.ToString("0.00") + " seconds cooldown" });
+                if (ability.Duration > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = ability.Duration.ToString("0.00") + " seconds cast" });
+
+                var timeCost = Math.Max(ability.Duration, ability.Cooldown.Duration);
+                if (ability.Damage.Value > 0 && (ability.Duration > 0 || ability.Cooldown.Duration > 0))
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageDamage / timeCost).ToString("0.00") + " DPS" });
+                if (ability.Damage.Value > 0 && (ability.Duration > 0))
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageDamage / ability.Duration).ToString("0.00") + " DPCT" });
+                if (ability.Healing.Value > 0 && (ability.Duration > 0 || ability.Cooldown.Duration > 0))
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageHealing / timeCost).ToString("0.00") + " HPS" });
+                if (ability.Damage.Value > 0 && ability.ThreatModifier > 1.0f && (ability.Duration > 0 || ability.Cooldown.Duration > 0))
+                    lines.Add(new Line { Color = Color.LightGray, Text = (ability.ThreatModifier * averageDamage / timeCost).ToString("0.00") + " TPS" });
+
+                if (ability.Damage.Value > 0 && ability.ManaCost > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageDamage / ability.ManaCost).ToString("0.00") + " DPM" });
+                if (ability.Damage.Value > 0 && ability.EnergyCost > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageDamage / ability.EnergyCost).ToString("0.00") + " DPE" });
+                if (ability.Healing.Value > 0 && ability.ManaCost > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageHealing / ability.ManaCost).ToString("0.00") + " HPM" });
+                if (ability.Healing.Value > 0 && ability.EnergyCost > 0)
+                    lines.Add(new Line { Color = Color.LightGray, Text = (averageHealing / ability.EnergyCost).ToString("0.00") + " HPE" });
+            }
+
+            Height = 80;
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(Vector2 position, GameTime gameTime)
+        {
+            if (lines.Any())
+            {
+                Height = 20 + (lines.Count + 2) * Font.LineSpacing;
+                Width = Math.Max(Width, Font.MeasureString(ability.Name).X + 20);
+                Width = Math.Max(Width, lines.Select(l => Font.MeasureString(l.Text).X).Max() + 20);
+
+                var bounds = new Rectangle((int)position.X, (int)position.Y - (int)Height, (int)Width, (int)Height);
+                var innerBounds = bounds;
+                innerBounds.Inflate(-1, -1);
+
+                SpriteBatch.Draw(BlankTexture, bounds, Color.White, ZIndex + 0.001f);
+                SpriteBatch.Draw(BlankTexture, innerBounds, new Color(20, 20, 20), ZIndex + 0.002f);
+
+                int x = (int)position.X + 10;
+                int y = (int)position.Y - (int)Height + 10;
+
+                SpriteBatch.DrawString(Font, ability.Name, new Vector2(x, y), Color.Yellow, ZIndex + 0.003f);
+                SpriteBatch.DrawString(Font, lines[0].Text, new Vector2(x, y += Font.LineSpacing), lines[0].Color, ZIndex + 0.003f);
+                if (ability.TargettingType != TargettingTypes.Self)
+                    SpriteBatch.DrawString(Font, ability.Range.ToString() + " meter range", new Vector2(x, y += Font.LineSpacing), GetRangeColor(actorBinding.GetValue()), ZIndex + 0.003f);
+                else
+                    Height -= Font.LineSpacing;
+
+                foreach (var line in lines.Skip(1))
+                {
+                    SpriteBatch.DrawString(Font, line.Text, new Vector2(x, y += Font.LineSpacing), line.Color, ZIndex + 0.003f);
+                }
+            }
+        }
+
+        private Color GetRangeColor(Actor actor)
+        {
+            if (!actor.Targets.Any())
+                return Color.LightGray;
+
+            var target = actor.Targets.Peek();
+            if (target.DistanceFrom(actor).In(ability.Range + actor.Radius + target.Radius))
+                return Color.LightGray;
+
+            return Color.Tomato;
+        }
+    }
+}
